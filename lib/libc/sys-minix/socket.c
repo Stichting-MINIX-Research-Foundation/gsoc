Stichting-MINIX-Research-Foundation/gsoc

commit 133ac063fd9e62965cca30393f9262eddc576bec

Fixed socket() to work with TCP over IPV6 Fixed a typo in the defintion of NWIOSTCP6CONF

lib/libc/sys-minix/socket.c
@@ -18,9 +18,10 @@ __weak_alias(socket, _socket)
 
 #define DEBUG 0
 
-static int _tcp_socket(int protocol);
+static int _tcp_socket(int domain, int protocol);
 static int _udp_socket(int protocol);
 static int _uds_socket(int type, int protocol);
+static 
 
 int socket(int domain, int type, int protocol)
 {
@@ -42,10 +43,13 @@ int socket(int domain, int type, int protocol)
 		return _uds_socket(type, protocol);
 
 	if (domain == AF_INET && type == SOCK_STREAM)
-		return _tcp_socket(protocol);
+		return _tcp_socket(domain, protocol);
 
 	if (domain == AF_INET && type == SOCK_DGRAM)
 		return _udp_socket(protocol);
+
+	if (domain == AF_INET6 && type == SOCK_STREAM)
+		return _tcp_socket(domain, protocol);
 
 #if DEBUG
 	fprintf(stderr, "socket: nothing for domain %d, type %d, protocol %d\n",
@@ -55,7 +59,7 @@ int socket(int domain, int type, int protocol)
 	return -1;
 }
 
-static int _tcp_socket(int protocol)
+static int _tcp_socket(int domain, int protocol)
 {
 	int fd;
 	if (protocol != 0 && protocol != IPPROTO_TCP)
@@ -66,7 +70,12 @@ static int _tcp_socket(int protocol)
 		errno= EPROTONOSUPPORT;
 		return -1;
 	}
-	fd= open(TCP_DEVICE, O_RDWR);
+
+	if (domain == AF_INET)
+		fd= open(TCP_DEVICE, O_RDWR);
+	else 
+		fd= open(TCP6_DEVICE, O_RDWR);
+
 	return fd;
 }
 

include/sys/ioc_net.h
@@ -98,8 +98,8 @@ struct msg_control
 #define NWIOSUDSRCVBUF	 _IOW('n', 95, size_t)            /* SO_RCVBUF */
 
 /* ipv6 sockets */
-#define NWIOSTCP6CONF	_IOW('n', 100, struct nwio_tcpconf)
-#define NWIOGTCP6CONF	_IOR('n', 101, struct nwio_tcpconf)
+#define NWIOSTCP6CONF	_IOW('n', 100, struct nwio_tcp6conf)
+#define NWIOGTCP6CONF	_IOR('n', 101, struct nwio_tcp6conf)
 #define NWIOTCP6CONN	_IOW('n', 102, struct nwio_tcpcl)
 #define NWIOTCP6LISTENQ	_IOW('n', 103, int)
 #define NWIOGTCP6COOKIE	_IOR('n', 104, struct tcp_cookie)

commit ec4786bf3fe9a9d8e9b66f113b0bae1afd757e0e

Minor change fixing a problem introduced by commit: 133ac063fd9e62965cca30393f9262eddc576bec

lib/libc/sys-minix/socket.c
@@ -21,7 +21,6 @@ __weak_alias(socket, _socket)
 static int _tcp_socket(int domain, int protocol);
 static int _udp_socket(int domain, int protocol);
 static int _uds_socket(int type, int protocol);
-static 
 
 int socket(int domain, int type, int protocol)
 {